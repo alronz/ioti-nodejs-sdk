a24766018dd3b2f26059a263ce9ecf7920ac5193
Bilal Al-Saeedi <bilal.alsaeedi@de.ibm.com>
2017-01-09 21:26:58 +0100

    add global client test

--- test/clients/GlobalClientTest.js
+++ test/clients/GlobalClientTest.js
@@ -1,18 +1,71 @@
 "use strict";
 
-describe('Global Client', function () {
+describe('Global Client', function() {
 
   var nock = require('nock');
   var should = require('chai').should();
   var IotIClient = require('../../src/ioti-client');
+  var baseUrl = "https://iot4insurance-api.eu-gb.mybluemix.net";
 
+  var configs =
+  {
+    uri: baseUrl,
+    userid: "testuserid",
+    password: "testpassword"
+  };
 
-  before(function () {
+  var iotIGlobal = new IotIClient.IotIGlobal(configs);
+  var testPayload = {"data": "value"};
+  var testPushNotification = {"data": "value"};
+
+  before(function() {
+
+    nock(baseUrl)
+    .post('/global/sendPushNotification', testPushNotification)
+    .reply(200, {"data": "value"});
+
+    nock(baseUrl)
+    .post('/global/sendPayloadToMQTT', testPayload)
+    .reply(200, {"data": "value"});
 
   });
 
-  after(function () {
+  after(function() {
+    nock.cleanAll();
+  });
 
+  describe('sendPushNotification()', function() {
+    it('should check no parameters provided', function() {
+      var result = iotIGlobal.sendPushNotification(null, function(error, body, response) {
+      });
+      result.should.be.a('error');
+    });
+
+    it('should send push notification successfully', function(done) {
+      iotIGlobal.sendPushNotification(testPushNotification, function(error, body, response) {
+        should.not.exist(error);
+        should.exist(body);
+        should.exist(response);
+        done();
+      });
+    });
+  });
+
+  describe('sendPayloadToMQTT()', function() {
+    it('should check no parameters provided', function() {
+      var result = iotIGlobal.sendPayloadToMQTT(null, null, null, null, null, function(error, body, response) {
+      });
+      result.should.be.a('error');
+    });
+
+    it('should send payload successfully', function(done) {
+      iotIGlobal.sendPayloadToMQTT(testPayload, function(error, body, response) {
+        should.not.exist(error);
+        should.exist(body);
+        should.exist(response);
+        done();
+      });
+    });
   });
 
 });
